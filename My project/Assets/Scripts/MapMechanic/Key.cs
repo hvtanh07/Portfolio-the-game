0115311609c7cafe6d23bb1e64406214b7112746
Tuấn Anh <32663098+hvtanh07@users.noreply.github.com>
2022-06-08 09:21:30 +0700

    small modification

--- My project/Assets/Scripts/MapMechanic/Key.cs
+++ My project/Assets/Scripts/MapMechanic/Key.cs
@@ -7,20 +7,28 @@ public class Key : MonoBehaviour
     [SerializeField]
     private KeyType keyType;
     private Animator anim;
+    private bool Acquired;
     public enum KeyType{
         Red,
         Blue,
         Green
     }
     private void Awake() {
+        Acquired = false;
         anim = GetComponent<Animator>();
     }
     public void AcquireKey(){
         anim.SetTrigger("gotKey");
+        Acquired = true;
+        GetComponent<Collider2D>().enabled = false;
         Destroy(gameObject,5f);
     }
     public KeyType GetKeyType(){
         return keyType;
     }
-
+    private void Update() {
+        if(Acquired){
+            transform.position += Vector3.up * Time.deltaTime;
+        }
+    }
 }
