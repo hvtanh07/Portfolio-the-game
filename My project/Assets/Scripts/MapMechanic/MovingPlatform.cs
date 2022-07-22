f0895c7fccaf072d5944673f22249a955e07e598
Tuấn Anh <hvtanh07@gmail.com>
2022-07-22 11:43:59 +0700

    adding moving crane

--- My project/Assets/Scripts/MapMechanic/MovingPlatform.cs
+++ My project/Assets/Scripts/MapMechanic/MovingPlatform.cs
@@ -29,7 +29,8 @@ public class MovingPlatform : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {       
-        other.transform.SetParent(transform);
+        activated = true;
+        other.transform.SetParent(transform);     
     }
     private void OnTriggerExit2D(Collider2D other) {
         other.transform.SetParent(null);
