169c345aa68948c387302ec09b01728e1cb204ab
Tuấn Anh <hvtanh07@gmail.com>
2022-10-27 18:13:11 +0700

    minor changed

---
+++ My project/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Unity.PixelGraphics_CodeGen.c
@@ -0,0 +1,133 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::Awake()
+extern void VelocityCamera_Awake_mE4C71A89E6F167F0AAC288567FE4A8685B36B616 (void);
+// 0x00000002 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::OnValidate()
+extern void VelocityCamera_OnValidate_m2F88B07C6A9673B5E47557E072C7D3788850B1C1 (void);
+// 0x00000003 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::OnEnable()
+extern void VelocityCamera_OnEnable_m29F42A724172BF4BFCB08F4B16D4AEA8A660A69A (void);
+// 0x00000004 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::OnDisable()
+extern void VelocityCamera_OnDisable_m790B6469B5A48FD9E5AECF3BE18DEC80E4FB4C2E (void);
+// 0x00000005 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::HandleBeginCameraRendering(UnityEngine.Rendering.ScriptableRenderContext,UnityEngine.Camera)
+extern void VelocityCamera_HandleBeginCameraRendering_m2D0A1DCF832031463FBA052E1B1D1EE339A363B3 (void);
+// 0x00000006 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::UpdateTextures(System.Boolean)
+extern void VelocityCamera_UpdateTextures_m2E0BEC6403B11AFE15A350F0A790DFD4CFF34E1E (void);
+// 0x00000007 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::Update()
+extern void VelocityCamera_Update_m3CC7A8D67EB03DF05A882E3744C765D6261449B7 (void);
+// 0x00000008 System.Void Aarthificial.PixelGraphics.Universal.VelocityCamera::.ctor()
+extern void VelocityCamera__ctor_mB461F680AF51628A378E2358D48DA54F0A66784F (void);
+// 0x00000009 System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderFeature::Create()
+extern void VelocityRenderFeature_Create_m8833711193D8B22891B11D53EE08D523714908B4 (void);
+// 0x0000000A System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderFeature::AddRenderPasses(UnityEngine.Rendering.Universal.ScriptableRenderer,UnityEngine.Rendering.Universal.RenderingData&)
+extern void VelocityRenderFeature_AddRenderPasses_mD9AEA264369EB93AA31D5BABDC9C1A92B9C2D5EE (void);
+// 0x0000000B System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderFeature::.ctor()
+extern void VelocityRenderFeature__ctor_mA9DBB19421DFAF271205957712CCFFD287DA6F35 (void);
+// 0x0000000C System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderPass::.ctor(UnityEngine.Material,UnityEngine.Material)
+extern void VelocityRenderPass__ctor_m8F751D6440A17D9A1A611A102F1DC64EB9944B77 (void);
+// 0x0000000D System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderPass::Setup(Aarthificial.PixelGraphics.Common.VelocityPassSettings,Aarthificial.PixelGraphics.Common.SimulationSettings)
+extern void VelocityRenderPass_Setup_mB004FC8001C686FEEA393540DA59ECEFAB41DA4F (void);
+// 0x0000000E System.Void Aarthificial.PixelGraphics.Forward.VelocityRenderPass::Execute(UnityEngine.Rendering.ScriptableRenderContext,UnityEngine.Rendering.Universal.RenderingData&)
+extern void VelocityRenderPass_Execute_mEAFA6E4DC0D54763040EB1778893AF71A7BF7031 (void);
+// 0x0000000F System.UInt32 Aarthificial.PixelGraphics.Common.RenderingLayerMask::op_Implicit(Aarthificial.PixelGraphics.Common.RenderingLayerMask)
+extern void RenderingLayerMask_op_Implicit_mC9097BB6B4335266F89AEBEAD3FE96B2AF5261BB (void);
+// 0x00000010 Aarthificial.PixelGraphics.Common.RenderingLayerMask Aarthificial.PixelGraphics.Common.RenderingLayerMask::op_Implicit(System.UInt32)
+extern void RenderingLayerMask_op_Implicit_mDED281A2236F22DB623A1DE0B0B3521B962B9171 (void);
+// 0x00000011 System.Void Aarthificial.PixelGraphics.Common.ShaderIds::.cctor()
+extern void ShaderIds__cctor_m3DD939058B3BB90B53AEA61BA82DCBBC568FC8CA (void);
+// 0x00000012 UnityEngine.Vector4 Aarthificial.PixelGraphics.Common.SimulationSettings::get_Value()
+extern void SimulationSettings_get_Value_mE3261134DF1ECA87030D1EAEEE741B51705C8AB3 (void);
+// 0x00000013 System.Void Aarthificial.PixelGraphics.Common.SimulationSettings::.ctor()
+extern void SimulationSettings__ctor_mAAD65DF4B738750AF0C3BB57A0549F5C8ADE0614 (void);
+// 0x00000014 System.Void Aarthificial.PixelGraphics.Common.VelocityEmitter::Awake()
+extern void VelocityEmitter_Awake_mEC715260947C896C448C64A434C070E762AE5A4D (void);
+// 0x00000015 System.Void Aarthificial.PixelGraphics.Common.VelocityEmitter::Update()
+extern void VelocityEmitter_Update_mF1CE3C3DC923605888D3A29D0CA4156479F53017 (void);
+// 0x00000016 System.Void Aarthificial.PixelGraphics.Common.VelocityEmitter::.ctor()
+extern void VelocityEmitter__ctor_m8F016006B913605E1A65A284C49F1992B47FBE15 (void);
+// 0x00000017 System.Void Aarthificial.PixelGraphics.Common.VelocityPassSettings::.ctor()
+extern void VelocityPassSettings__ctor_m3A285CAEFC7D7A36AD6F6F1F54A6C739011ED135 (void);
+static Il2CppMethodPointer s_methodPointers[23] = 
+{
+	VelocityCamera_Awake_mE4C71A89E6F167F0AAC288567FE4A8685B36B616,
+	VelocityCamera_OnValidate_m2F88B07C6A9673B5E47557E072C7D3788850B1C1,
+	VelocityCamera_OnEnable_m29F42A724172BF4BFCB08F4B16D4AEA8A660A69A,
+	VelocityCamera_OnDisable_m790B6469B5A48FD9E5AECF3BE18DEC80E4FB4C2E,
+	VelocityCamera_HandleBeginCameraRendering_m2D0A1DCF832031463FBA052E1B1D1EE339A363B3,
+	VelocityCamera_UpdateTextures_m2E0BEC6403B11AFE15A350F0A790DFD4CFF34E1E,
+	VelocityCamera_Update_m3CC7A8D67EB03DF05A882E3744C765D6261449B7,
+	VelocityCamera__ctor_mB461F680AF51628A378E2358D48DA54F0A66784F,
+	VelocityRenderFeature_Create_m8833711193D8B22891B11D53EE08D523714908B4,
+	VelocityRenderFeature_AddRenderPasses_mD9AEA264369EB93AA31D5BABDC9C1A92B9C2D5EE,
+	VelocityRenderFeature__ctor_mA9DBB19421DFAF271205957712CCFFD287DA6F35,
+	VelocityRenderPass__ctor_m8F751D6440A17D9A1A611A102F1DC64EB9944B77,
+	VelocityRenderPass_Setup_mB004FC8001C686FEEA393540DA59ECEFAB41DA4F,
+	VelocityRenderPass_Execute_mEAFA6E4DC0D54763040EB1778893AF71A7BF7031,
+	RenderingLayerMask_op_Implicit_mC9097BB6B4335266F89AEBEAD3FE96B2AF5261BB,
+	RenderingLayerMask_op_Implicit_mDED281A2236F22DB623A1DE0B0B3521B962B9171,
+	ShaderIds__cctor_m3DD939058B3BB90B53AEA61BA82DCBBC568FC8CA,
+	SimulationSettings_get_Value_mE3261134DF1ECA87030D1EAEEE741B51705C8AB3,
+	SimulationSettings__ctor_mAAD65DF4B738750AF0C3BB57A0549F5C8ADE0614,
+	VelocityEmitter_Awake_mEC715260947C896C448C64A434C070E762AE5A4D,
+	VelocityEmitter_Update_mF1CE3C3DC923605888D3A29D0CA4156479F53017,
+	VelocityEmitter__ctor_m8F016006B913605E1A65A284C49F1992B47FBE15,
+	VelocityPassSettings__ctor_m3A285CAEFC7D7A36AD6F6F1F54A6C739011ED135,
+};
+static const int32_t s_InvokerIndices[23] = 
+{
+	5299,
+	5299,
+	5299,
+	5299,
+	2413,
+	4158,
+	5299,
+	5299,
+	5299,
+	2355,
+	5299,
+	2372,
+	2372,
+	2411,
+	9112,
+	9014,
+	10041,
+	5292,
+	5299,
+	5299,
+	5299,
+	5299,
+	5299,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_PixelGraphics_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_PixelGraphics_CodeGenModule = 
+{
+	"Unity.PixelGraphics.dll",
+	23,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
