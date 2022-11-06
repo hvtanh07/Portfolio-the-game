da598f850de754e52864dc359ab6f972e5bad3dc
Tuấn Anh <32663098+hvtanh07@users.noreply.github.com>
2022-11-06 21:52:54 +0700

    minor change

--- My project/Assets/Scripts/MapMechanic/KeyDoor.cs
+++ My project/Assets/Scripts/MapMechanic/KeyDoor.cs
@@ -18,16 +18,22 @@ public class KeyDoor : MonoBehaviour
     }
     public void OpenDoor()
     {
-        if (keyType == Key.KeyType.Red)
+        switch (keyType)
         {
-            LeanTween.move(this.gameObject, targetDir, speed).setOnComplete(stopAudio);
-            au.Play();
+            case Key.KeyType.Red:
+                {
+                    LeanTween.move(this.gameObject, targetDir, speed).setOnComplete(stopAudio);
+                    au.Play();
+                    break;
+                }
+            case Key.KeyType.Blue:
+                {
+                    LeanTween.rotateAroundLocal(this.gameObject, Vector3.forward, 90, speed);
+                    GetComponent<BoxCollider2D>().enabled = false;
+                    break;
+            }
         }
-        else if (keyType == Key.KeyType.Blue)
-        {
-            LeanTween.rotateAroundLocal(this.gameObject, Vector3.forward,90, speed);
-            GetComponent<BoxCollider2D>().enabled = false;
-        }
+        
         unlocked = true;
 
 
